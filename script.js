4dfa63cd255053734538dfaa629630843efdff5d
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-10-07 02:28:12 +0600

    Game start and end both are successfully done.

--- script.js
+++ script.js
@@ -1,48 +1,86 @@
 'use strict';
 
 // select element
-const score0El=document.querySelector('#score--0');
-const score1El=document.getElementById('score--1');
-const diceEl=document.querySelector('.dice');
-const current0El=document.getElementById('current--0');
-const current1El=document.getElementById('current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
 // select event
-const btnNew=document.querySelector('.btn--new');
-const btnRoll=document.querySelector('.btn--roll');
-const btnHold=document.querySelector('.btn--hold');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // starting condition
-score0El.textContent=0;
-score1El.textContent=0;
+score0El.textContent = 0;
+score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+let currentScore = 0;
+let activePlayer = 0;
+let playing = true;
+const score = [0, 0];
 
+// switch to the next player / -> refactoring code
+const switchPlayer = () => {
+  // switch to the next player
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  // reassign active player value
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
-let currentScore=0;
-let activePlayer=0;
-//  rolling dice 
-btnRoll.addEventListener('click', ()=>{
-    const dice= Math.trunc(Math.random()*6)+1;
+//  rolling dice
+btnRoll.addEventListener('click', () => {
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
     console.log(dice);
 
     // display dice
     diceEl.classList.remove('hidden');
-    diceEl.src=`dice-${dice}.png`;
+    diceEl.src = `dice-${dice}.png`;
 
     // check for roll 1
     // if.. we did not get 1 then add all dice point
     // else.. when get 1 then switch to the next player by reassign the activePlayer value..like at first.. activePlayer===0 that is true so assign activePlayer value 1  .
-    if(dice !== 1){
-        // add dice to current score
-       currentScore+=dice;
-       document.getElementById(`current--${activePlayer}`).textContent=currentScore;
-
-    }else{
-        // switch to the next player
-        document.getElementById(`current--${activePlayer}`).textContent=0;
-        currentScore=0;
-        // reassign active player value
-        activePlayer=activePlayer === 0 ? 1 : 0;
+    if (dice !== 1) {
+      // add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
     }
-})
+  }
+});
+
+btnHold.addEventListener('click', () => {
+  if (playing) {
+    // 1. add current score to active player score
+    console.log(currentScore);
+    score[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      score[activePlayer];
+    // 2. check if player score >=100
+    if (score[activePlayer] >= 50) {
+        playing=false;
+        diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // switch to the next player
+      switchPlayer();
+    }
+  }
+});
+// finish the game
